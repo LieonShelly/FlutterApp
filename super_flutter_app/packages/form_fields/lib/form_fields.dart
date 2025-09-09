52978ff6afa64fbc170d58c45c5077db17233b55
lieon <lieoncx@gmail.com>
2025-09-09 16:33:35 +0800

    Update sign_in_cubit

--- super_flutter_app/packages/form_fields/lib/form_fields.dart
+++ super_flutter_app/packages/form_fields/lib/form_fields.dart
@@ -1,4 +1,5 @@
 export 'src/email.dart';
 export 'src/password.dart';
-export 'package:formz/formz.dart' show FormzInput, FormzStatus, FormzStatusX;
+export 'package:formz/formz.dart'
+    show FormzInput, FormzStatus, Formz, FormzStatusX;
 export 'src/username.dart';
