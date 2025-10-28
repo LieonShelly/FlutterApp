76b84aa5aca3b807b6ee1e6538cac6b81c9ca0d5
lieon <lieoncx@gmail.com>
2025-10-28 17:42:37 +0800

    Update

---
+++ super_flutter_app/packages/component_library/lib/theme/wonder_theme_data.dart
@@ -0,0 +1,27 @@
+import 'package:component_library/theme/wonder_theme.dart';
+import 'package:flutter/material.dart';
+
+class WonderTheme extends InheritedWidget {
+  final WonderThemeData lightTheme;
+  final WonderThemeData darkTheme;
+
+  const WonderTheme({
+    required Widget child,
+    required this.lightTheme,
+    required this.darkTheme,
+    Key? key,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(WonderTheme oldWidget) =>
+      oldWidget.lightTheme != lightTheme || oldWidget.darkTheme != darkTheme;
+
+  static WonderThemeData of(BuildContext context) {
+    final WonderTheme? inheritedTheme = context
+        .dependOnInheritedWidgetOfExactType<WonderTheme>();
+    final currentBrightness = Theme.of(context).brightness;
+    return currentBrightness == Brightness.dark
+        ? inheritedTheme!.darkTheme
+        : inheritedTheme!.lightTheme;
+  }
+}
