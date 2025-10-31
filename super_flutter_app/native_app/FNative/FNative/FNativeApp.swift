6dbde8db3be382192857858bb2d607ad1f350927
lieon <lieoncx@gmail.com>
2025-11-01 00:36:38 +0800

    Update

---
+++ super_flutter_app/native_app/FNative/FNative/FNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FNativeApp.swift
+//  FNative
+//
+//  Created by Renjun Li on 2025/11/1.
+//
+
+import SwiftUI
+
+@main
+struct FNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ super_flutter_app/native_app/FNative/Pods/Target Support Files/Pods-FNative-FNativeUITests/Pods-FNative-FNativeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FNative_FNativeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FNative_FNativeUITests
+@end
